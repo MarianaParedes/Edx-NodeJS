b787cc46c8e52c1c549d29c877db3358b9c26165
Mariana <marian.paredes.gee@gmail.com>
2024-04-08 15:43:05 -0300

    Enhancing an http server with websockets < (8)

--- static/app.js
+++ static/app.js
@@ -1,59 +1,96 @@
-'use strict'
-
-// API URL
-const API = 'http://localhost:3000'
-
-// Populate products from API method
-const populateProducts = async (category, method = 'GET', payload) => { // a populateProducts le asigna una funcion asincrona con los params categoria, metodo, carga útil
-  const products = document.querySelector('#products') //toma el selector productsde un div
-  products.innerHTML = '' //lo pone en blanco
-  const send = method === 'GET' ? {} : {  //en una constante send, metodo verifica si es get, entonces un obj vacio, sino envia los headers y en el body envia un JSON con el param payload
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(payload)
-  }
-  const res = await fetch(`${API}/${category}`, { method, ...send }) // 1) a una constante RES de response le asigna una funcion asincrona fetch ? (método)  API/categoria, con los parámetros(?) método y send
-                                                                     // 2 se ejecuta el fetch trayendo data que despues es tratada     
-  const data = await res.json()  // 1) a la const data lo toma como asincrono pasandole el res de arriba como json 2) el res lo convierte a json para usarlo abajo 3)render data from the response
-  for (const product of data) {
-    const item = document.createElement('product-item')
-    for (const key of ['name', 'rrp', 'info']) {
-      const span = document.createElement('span')
-      span.slot = key
-      span.textContent = product[key]
-      item.appendChild(span)
-    }
-    products.appendChild(item)
-  }
-}
-
-// Get elements from DOM
-const category = document.querySelector('#category')
-const add = document.querySelector('#add')
+const API = "http://localhost:3000";
+const WS_API = "ws://localhost:3000";
 
 // Populate products
-category.addEventListener('input', async ({ target }) => { //Llama a populateProducts cuando la categoria se cambia
-  add.style.display = 'block'
-  await populateProducts(target.value) //este target no se que onda target.value parece ser el valor del select
-})
+const populateProducts = async (category, method = "GET", payload) =>
+{
+  const products = document.querySelector("#products");
+  products.innerHTML = "";
+  // Send request
+  const send =
+    method === "GET"
+      ? {}
+      : {
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        };
+  // Get data
+  const res = await fetch(`${API}/${category}`, { method, ...send });
+  const data = await res.json();
+  // Populate products
+  for (const product of data) {
+    const item = document.createElement("product-item");
+    item.dataset.id = product.id;
+    for (const key of ["name", "rrp", "info"]) {
+      const span = document.createElement("span");
+      span.slot = key;
+      span.textContent = product[key];
+      item.appendChild(span);
+  }
+  // Append to DOM
+  products.appendChild(item);
+ }
+};
 
-// Add product
-add.addEventListener('submit', async (e) => {
-  e.preventDefault()
-  const { target } = e //el evento?
+// Get Elements from DOM
+const category = document.querySelector("#category");
+const add = document.querySelector("#add");
+
+let socket = null;
+// Realtime orders handler using WebSocket
+const realtimeOrders = (category) => {
+  if (socket) socket.close();
+  socket = new WebSocket(`${WS_API}/orders/${category}`);
+  socket.addEventListener("message", ({ data }) => {
+    try {
+      console.log(data)
+      const { id, total } = JSON.parse(data);
+      const item = document.querySelector(`[data-id="${id}"]`);
+      if (item === null) return;
+      const span =
+        item.querySelector('[slot="orders"]')
+         || document.createElement("span");
+      span.slot = "orders";
+      span.textContent = total;
+      item.appendChild(span);
+    } catch (err) {
+      console.error('Hola')
+      console.error(err);
+    }
+  });
+};
+
+// Populate products on page load
+category.addEventListener("input", async ({ target }) => {
+  add.style.display = "block";
+  await populateProducts(target.value);
+  realtimeOrders(target.value);
+});
+
+// Add product form handler
+add.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const { target } = e;
   const payload = {
     name: target.name.value,
     rrp: target.rrp.value,
-    info: target.info.value
-  }
-  await populateProducts(category.value, 'POST', payload)
-  target.reset() //no se que onda
-})
+    info: target.info.value,
+  };
+  await populateProducts(category.value, "POST", payload);
+  realtimeOrders(category.value);
+  // Reset form
+  target.reset();
+});
 
-// Custom element
-customElements.define('product-item', class Item extends HTMLElement {
-  constructor() {
-    super()
-    const itemTmpl = document.querySelector('#item').content
-    this.attachShadow({mode: 'open'}).appendChild(itemTmpl.cloneNode(true))
+// Define custom element
+customElements.define(
+  "product-item",
+  class Item extends HTMLElement {
+    constructor() {
+      super();
+      const itemTmpl = document.querySelector("#item").content;
+      this.attachShadow({ mode: "open"
+}).appendChild(itemTmpl.cloneNode(true));
+    }
   }
-})
+);
